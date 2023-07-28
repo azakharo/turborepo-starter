5ab72e86a820749d9d67ccc196ee52459e14d3ce
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2023-07-28 17:13:12 +0300

    dummy commit

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,10 @@
 import {Button, Header} from 'ui';
 
-let myfunc2 = (a, b) => a + b;
+//let myfunc2 = (a, b) => a + b;
 
 export default function Page() {
-  let myvar3 = 1;
-  console.log(
-'ha-ha-ha');
-  console.log('ha2');
-
+  const myvar3 = 1;
+  console.log('ha-ha-ha', myvar3);
 
   return (
     <>
@@ -16,4 +13,3 @@ export default function Page() {
     </>
   );
 }
-
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  // Type check TypeScript files
-  'src/**/*.(ts|tsx)': () => 'npm run type-check',
-
-  // Lint then format TypeScript and JavaScript files
-  'src/**/*.(ts|tsx|js|jsx)': filenames => [
-    `npm run lint`,
-    `npm run code-format`,
+  '(apps|packages)/**/*': () => [
+    'npm run type-check',
+    'npm run lint',
+    'npm run code-format',
   ]
 };
